dc31a059f823b845150c4c802d751b4fec740877
93 <84161489+4l3xu@users.noreply.github.com>
2021-05-28 03:02:12 +0300

    Add files via upload

---
+++ fatorial.cpp
@@ -0,0 +1,24 @@
+﻿// 10. Дано натуральное число n.Написать программу, которая вычисляет факториал неотрицательных целых чисел n(т.е.число целое и больше 0).
+
+#include <iostream>
+using namespace std;
+
+void main()
+{
+	int a, f = 1, i = 1;
+
+	cout << "vvedite chislo bolshe 0 ";
+	cin >> a;
+
+	do
+	{
+
+		f *= i;
+		i++;
+		cout << "facktorial = " << f << endl;
+		cout << "i = " << f << endl;
+
+	} while (i <= a);
+
+	cout << "facktorial = " << f << endl;
+}
