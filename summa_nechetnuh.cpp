5a4e46d08e6254447fdf330b543a39582f332d0c
93 <84161489+4l3xu@users.noreply.github.com>
2021-05-28 03:03:25 +0300

    Add files via upload

---
+++ summa_nechetnuh.cpp
@@ -0,0 +1,54 @@
+﻿//5. Написать программу, которая находит сумму всех целых нечетных чисел в диапазоне, указанном пользователе.
+
+#include <iostream>
+using namespace std;
+
+void main()
+{
+	int a, b, min, max, sum = 0, sum2, i = 0;
+
+	cout << "vvedite 2 chisla: ";
+	cin >> a >> b;
+	if (a > b)
+	{
+		min = b;
+		max = a;
+
+
+	}
+	else if (a < b)
+	{
+		max = b;
+		min = a;
+
+
+	}
+	else
+	{
+		cout << "summa chisel:" << a;
+	}
+
+	cout << "minimalnoe chislo: " << min << endl;
+	cout << "maksimalnoe chislo: " << max << endl;
+
+
+	// что с глобальным и локальным пространством????
+
+
+	do
+	{
+		if (min % 2 != 0)
+		{
+			sum += min;
+			cout << "min = " << min << endl;
+			cout << "sum = " << sum << endl;
+
+			// переменные выводятся для проверки
+		}
+		min++;
+	} 	while (min <= max);
+
+
+
+	cout << "summa chisel: " << sum << endl;
+}
